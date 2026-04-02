e066a29d96e682ee13bed479d9de350e7a99a26c
sani <iamumersani@gmail.com>
2026-04-02 05:53:07 +0500

    Revert "feat: Update VideoStreamWidget to support raw camera stream and add reset zoom functionality"
    This reverts commit d9b6906f18390681ccdbd7ec6121d11d98bd0f4c.

--- desktop-client/src/ui/widgets/VideoStreamWidget.h
+++ desktop-client/src/ui/widgets/VideoStreamWidget.h
@@ -27,13 +27,14 @@ public:
 
 protected:
     void resizeEvent(QResizeEvent *event) override;
+    void showEvent(QShowEvent *event) override;
+    void hideEvent(QHideEvent *event) override;
 
 private slots:
-    void onImageReceived(const QByteArray& imageData, int width, int height);
+    void onImageReceived(const QString& topic, const QByteArray& imageData, int width, int height);
     void onStreamTabChanged(int index);
     void onZoomInClicked();
     void onZoomOutClicked();
-    void onResetZoomClicked();
     void onToggleRecording();
     void onRobotStatusUpdated(const QString& status);
 
@@ -47,7 +48,6 @@ private:
     QTabBar* m_tabBar;
     QPushButton* m_zoomInButton;
     QPushButton* m_zoomOutButton;
-    QPushButton* m_resetZoomButton;
     QLabel* m_topicOverlay;
     QLabel* m_resolutionOverlay;
     QLabel* m_statusOverlay;
