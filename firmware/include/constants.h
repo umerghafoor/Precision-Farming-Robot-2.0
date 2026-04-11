f327b73441ff30d4a84a302c37219e9117ae5cc7
Umer Ghafoor <umerghafoor@gmail.com>
2026-04-11 06:35:28 +0500

    feat: Refactor motor control to use AFMotor library and update platformio configuration

--- firmware/include/constants.h
+++ firmware/include/constants.h
@@ -1,29 +1,11 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-// L298N Motor Driver 1 Pins (Left Side)
-#define MOTOR1_IN1 2   // Digital pin 2 -> Motor 1 IN1
-#define MOTOR1_IN2 3   // Digital pin 3 -> Motor 1 IN2
-#define MOTOR1_ENA 5   // PWM pin 5 -> Motor 1 speed control
-
-#define MOTOR2_IN1 4   // Digital pin 4 -> Motor 2 IN1
-#define MOTOR2_IN2 7   // Digital pin 7 -> Motor 2 IN2
-#define MOTOR2_ENB 6   // PWM pin 6 -> Motor 2 speed control
-
-// L298N Motor Driver 2 Pins (Right Side)
-#define MOTOR3_IN1 8   // Digital pin 8 -> Motor 3 IN1
-#define MOTOR3_IN2 9   // Digital pin 9 -> Motor 3 IN2
-#define MOTOR3_ENA 10  // PWM pin 10 -> Motor 3 speed control
-
-#define MOTOR4_IN1 11  // Digital pin 11 -> Motor 4 IN1
-#define MOTOR4_IN2 12  // Digital pin 12 -> Motor 4 IN2
-#define MOTOR4_ENB A0  // Analog pin A0 used as PWM -> Motor 4 speed control
-
-// SPI Hardware Pins on Arduino Nano
-// MISO = Pin 12
-// MOSI = Pin 11
-// SCK = Pin 13
-// SS = Pin 10
+// AFMotor channel mapping (Adafruit Motor Shield v1)
+#define MOTOR1_INDEX 1
+#define MOTOR2_INDEX 2
+#define MOTOR3_INDEX 3
+#define MOTOR4_INDEX 4
 
 #define LED_PIN 13     // Onboard LED
 
